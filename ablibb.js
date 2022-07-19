a2c2eafaaa47bd415422fc081e068e82b3cf7688
sanjinxiaolongxia <67628874+sanjinxiaolongxia@users.noreply.github.com>
2022-07-19 16:00:20 +0800

    Update ablibb.js

--- ablibb.js
+++ ablibb.js
@@ -32,7 +32,7 @@ const codeJosn= {
   "KScode":"##Y44WaunVk##",
   //"KScode":"##X-36rL6xvSuEZ1PD##",
   "TBcode":"(OJ7L2LfETOj¢/",
-  "DYcode":"ζkxbioj1w5u8ÊÊ",
+  "DYcode":"ㄍㄍjBTmZ3AL6u8ξ",
   "Otcode":""
 }
 let cpCode = '';
