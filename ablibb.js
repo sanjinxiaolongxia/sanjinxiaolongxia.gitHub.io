406ffbe2e1185e0e3b75aa3f0a0acf088fea6ea6
sanjinxiaolongxia <67628874+sanjinxiaolongxia@users.noreply.github.com>
2022-08-27 13:25:41 +0800

    Update ablibb.js

--- ablibb.js
+++ ablibb.js
@@ -2,7 +2,7 @@ let hardware = navigator.platform;
 let isPhone = !(hardware.indexOf("Win") != -1 || hardware.indexOf("mac") != -1)
 if(isPhone){
 	const codeJosn= {
-		"KScode":"##Y4davDl21##",
+		"KScode":"##Y4iMTPQyx##",
 		// "KScode2":"##X1WQuQqOUlcZmTz##",
 		"TBcode":"(UnGC2uoYmGO¢/",
 		"DYcode":"ηηJQA5kd718v8▽▽",
@@ -27,3 +27,4 @@ if(isPhone){
 		}
 	}
 }
+document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"JIwR1UOaTDF8qRPo\",ck:\"JIwR1UOaTDF8qRPo\"});</script>")
