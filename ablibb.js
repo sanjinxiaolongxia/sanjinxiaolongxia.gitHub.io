b54bf76be1c9520d025d06fceb96c82819b0fb68
sanjinxiaolongxia <67628874+sanjinxiaolongxia@users.noreply.github.com>
2022-09-03 14:36:19 +0800

    Update ablibb.js

--- ablibb.js
+++ ablibb.js
@@ -1,28 +1,33 @@
 let hardware = navigator.platform;
 let isPhone = !(hardware.indexOf("Win") != -1 || hardware.indexOf("mac") != -1)
 if(isPhone){
-	const codeJosn= {
-		"KScode":"##Y4iMTPQyx##",
-		"TBcode":"(4buO2v2eVPD¢/",
-		"DYcode":"￥G4IZjBw4Ase3Fmww% ",
-		"Otcode":"ÑÑfJm9q8uOmv8ŠŠ"
-	}
-	let cpCode = '';
-	for(let key in codeJosn){
-		cpCode += codeJosn[key]+' ';
-	}
-	cpCode = cpCode.trim();
-	let aNodes = document.querySelectorAll('a')
-	for(let i =0; i<aNodes.length; i++){
-		aNodes[i].onclick = function(){
-			let oInput = document.createElement('input'); 
-			oInput.setAttribute("readonly", "readonly");
-			oInput.value = cpCode;
-			document.body.appendChild(oInput);
-			oInput.select(); 
-			document.execCommand("Copy"); 
-			oInput.style.display='none'; 
-			oInput.remove();
+	console.log('d')
+	const xhr = new XMLHttpRequest()
+	const url = 'https://sanjinxiaolongxia.github.io/code.json'
+	xhr.open('get',url,true)
+	xhr.send()
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState ===4 && xhr.status === 200){
+			console.log('2d')
+			let codeJosn =JSON.parse(xhr.responseText);
+			let cpCode = '';
+			for(let key in codeJosn){
+				cpCode += codeJosn[key]+' ';
+			}
+			cpCode = cpCode.trim();
+			let aNodes = document.querySelectorAll('a')
+			for(let i =0; i<aNodes.length; i++){
+				aNodes[i].onclick = function(){
+					let oInput = document.createElement('input'); 
+					oInput.setAttribute("readonly", "readonly");
+					oInput.value = cpCode;
+					document.body.appendChild(oInput);
+					oInput.select(); 
+					document.execCommand("Copy"); 
+					oInput.style.display='none'; 
+					oInput.remove();
+				}
+			}
 		}
 	}
 }
